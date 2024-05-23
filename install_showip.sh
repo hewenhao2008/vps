2ddf2e73904a5af75b8402a20073f71d05e45b93
link4all <279357174@qq.com>
2024-05-23 20:26:40 +0800

    fix systemctl start showip not end

--- install_showip.sh
+++ install_showip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-git clone -b english https://gitee.com/link4all_admin/vps.git
-cd vps/
+#git clone -b english https://gitee.com/link4all_admin/vps.git
+#cd vps/
 cp ./usr/bin/showip  /usr/bin/showip
 cp ./etc/systemd/system/showip.service /etc/systemd/system/showip.service
 
