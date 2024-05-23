4421aa2502c3168d37083b3dcd8051bf1a7449e8
link4all <279357174@qq.com>
2024-05-23 20:14:54 +0800

    fix install_showip.sh

--- install_showip.sh
+++ install_showip.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 git clone -b english https://gitee.com/link4all_admin/vps.git
-
-cp vps/usr/bin/showip  /usr/bin/showip
-cp vps/etc/systemd/system/showip.service /etc/systemd/system/showip.service
+cd vps/
+cp ./usr/bin/showip  /usr/bin/showip
+cp ./etc/systemd/system/showip.service /etc/systemd/system/showip.service
 
 systemctl enable showip.service
 systemctl start showip.service
